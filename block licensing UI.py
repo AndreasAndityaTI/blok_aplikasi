c4e9a699222d8d9760d54208d5271231811b5b46
AndreasAndityaTI <93309168+AndreasAndityaTI@users.noreply.github.com>
2022-07-31 04:18:28 +0700

    ngasih komen di baris 1
    LicensingUI.exe

--- block licensing UI.py
+++ block licensing UI.py
@@ -1,4 +1,4 @@
-aplikasi=input("Masukkan nama aplikasi : ")
+aplikasi=input("Masukkan nama aplikasi : ") #LicensingUI.exe
 import os
 while(True):
     os.system(f"taskkill /im {aplikasi}") # select process by its name
